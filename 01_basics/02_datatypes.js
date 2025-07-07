382f08c51c196ed65af956ccd625f36b624f501c
Tanvi062005 <133089237+Tanvi062005@users.noreply.github.com>
2025-07-07 09:04:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log ("Tanvi")
+let name = "Tanvi"
+let age = 18
+let isLoggedIn = false
+
+
+console.log (typeof "Tanvi")
+console.log (typeof age)
+console.log (typeof undefined)
+console.log (typeof null)
